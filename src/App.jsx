c3f96662c236af2d0cbc3d5806e167a11e186e43
Isiak Quadri Opeyemi <opequadri2@gmail.com>
2025-05-06 16:41:39 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import Sidebar from "./components/sidebar";
+import Sidebar from "./components/Sidebar";
 import AppRoutes from "./Routes/AppRoutes";
 
 export default function App() {
@@ -8,9 +8,8 @@ export default function App() {
     <div className="px-5 md:px-20 lg:px-30 h-screen bg-gray-50 dark:bg-gray-900">
       <div className="flex gap-5 h-full">
         {/* Persistent Sidebar */}
-        <div>
-          <Sidebar />
-        </div>
+
+        <Sidebar />
 
         {/* Main Content Area */}
         <div className="flex-1 overflow-y-auto -ml-5 md:ml-0 lg:ml-0">
